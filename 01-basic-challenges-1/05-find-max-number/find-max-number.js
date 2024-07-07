9d6dea1910541f5e2473f22a040aa1ef153a68b0
chartdesign <chartshorne@gmail.com>
2024-07-07 16:58:46 -0700

    still goin

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
+function findMaxNumber(numArray) {
+  let maxNumber = 0;
+  for (let index = 0; index < numArray.length; index++) {
+    let element = numArray[index];
+    if (element > maxNumber) {
+      maxNumber = element;
+    }
+  }
+  return maxNumber;
+}
 
 module.exports = findMaxNumber;
