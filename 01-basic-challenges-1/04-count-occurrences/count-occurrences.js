b3d7831ad2ddb0e96cdfad36222868fd0cf54701
chartdesign <chartshorne@gmail.com>
2024-07-07 16:49:48 -0700

    easy stuff

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,12 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let count = 0;
+  for (let index = 0; index < str.length; index++) {
+    const element = str[index];
+    if (element === char) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countOccurrences;
